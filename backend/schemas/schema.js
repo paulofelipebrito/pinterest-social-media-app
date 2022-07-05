c9bdfcfd317612365497c732cd5e57812d253bee
Paulo Felipe Brito <paulofelipebrito@hotmail.com>
2022-07-04 21:45:30 -0300

    feat: backend_pin_schema

--- backend/schemas/schema.js
+++ backend/schemas/schema.js
@@ -11,5 +11,5 @@ export default createSchema({
   name: 'default',
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
-  types: schemaTypes.concat([pin, user, postedBy, comment, save]),
+  types: schemaTypes.concat([user]),
 })
